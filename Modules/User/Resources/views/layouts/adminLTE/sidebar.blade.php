610ab8da458beaaddc05374fccf282b3201cd4df
Marcelo Cuba <marcelo.cuba.10@hotmail.com>
2022-08-19 17:43:24 -0400

    FIX sidebar module user

--- Modules/User/Resources/views/layouts/adminLTE/sidebar.blade.php
+++ Modules/User/Resources/views/layouts/adminLTE/sidebar.blade.php
@@ -17,16 +17,46 @@
             <span class="text">Dashboard</span>
           </a>
         </li>
-        @can('customer-list')
-        <li class="nav-item {{ (request()->is('user/customers')) ? 'active' : '' }}">
-          <a href="/user/customers">
+        @can('financial-list')
+        <li class="nav-item nav-item-has-children">
+          <a aria-expanded="false" class="collapsed" id="ddlink_1" href="#" onclick="toggle('ddmenu_1', 'ddlink_1')">
             <span class="icon">
               <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M12,5A3.5,3.5 0 0,0 8.5,8.5A3.5,3.5 0 0,0 12,12A3.5,3.5 0 0,0 15.5,8.5A3.5,3.5 0 0,0 12,5M12,7A1.5,1.5 0 0,1 13.5,8.5A1.5,1.5 0 0,1 12,10A1.5,1.5 0 0,1 10.5,8.5A1.5,1.5 0 0,1 12,7M5.5,8A2.5,2.5 0 0,0 3,10.5C3,11.44 3.53,12.25 4.29,12.68C4.65,12.88 5.06,13 5.5,13C5.94,13 6.35,12.88 6.71,12.68C7.08,12.47 7.39,12.17 7.62,11.81C6.89,10.86 6.5,9.7 6.5,8.5C6.5,8.41 6.5,8.31 6.5,8.22C6.2,8.08 5.86,8 5.5,8M18.5,8C18.14,8 17.8,8.08 17.5,8.22C17.5,8.31 17.5,8.41 17.5,8.5C17.5,9.7 17.11,10.86 16.38,11.81C16.5,12 16.63,12.15 16.78,12.3C16.94,12.45 17.1,12.58 17.29,12.68C17.65,12.88 18.06,13 18.5,13C18.94,13 19.35,12.88 19.71,12.68C20.47,12.25 21,11.44 21,10.5A2.5,2.5 0 0,0 18.5,8M12,14C9.66,14 5,15.17 5,17.5V19H19V17.5C19,15.17 14.34,14 12,14M4.71,14.55C2.78,14.78 0,15.76 0,17.5V19H3V17.07C3,16.06 3.69,15.22 4.71,14.55M19.29,14.55C20.31,15.22 21,16.06 21,17.07V19H24V17.5C24,15.76 21.22,14.78 19.29,14.55M12,16C13.53,16 15.24,16.5 16.23,17H7.77C8.76,16.5 10.47,16 12,16Z" />
+                <path fill="currentColor" d="M21.04 12.13C21.18 12.13 21.31 12.19 21.42 12.3L22.7 13.58C22.92 13.79 22.92 14.14 22.7 14.35L21.7 15.35L19.65 13.3L20.65 12.3C20.76 12.19 20.9 12.13 21.04 12.13M19.07 13.88L21.12 15.93L15.06 22H13V19.94L19.07 13.88M11 19L9 21H5C3.9 21 3 20.1 3 19V5C3 3.9 3.9 3 5 3H9.18C9.6 1.84 10.7 1 12 1C13.3 1 14.4 1.84 14.82 3H19C20.1 3 21 3.9 21 5V9L19 11V5H17V7H7V5H5V19H11M12 3C11.45 3 11 3.45 11 4C11 4.55 11.45 5 12 5C12.55 5 13 4.55 13 4C13 3.45 12.55 3 12 3Z" />
               </svg>
             </span>
-            <span class="text">Clientes</span>
+            <span class="text">Registros</span>
           </a>
+          <ul id="ddmenu_1" class="dropdown-nav" style="{{ (request()->is('user/reports/*')) ? '' : 'display:none' }}">
+            @can('customer-list')
+            <li >
+              <a href="/user/customers" class="{{ (request()->is('user/records/customers')) ? 'active' : '' }}">Cliente</a>
+            </li>
+            @endcan
+            <li >
+              <a href="/user/providers" class="{{ (request()->is('user/records/providers')) ? 'active' : '' }}">Proveedor</a>
+            </li>
+          </ul>
+        </li>
+        @endcan
+        @can('financial-list')
+        <li class="nav-item nav-item-has-children">
+          <a aria-expanded="false" class="collapsed" id="ddlink_2" href="#" onclick="toggle('ddmenu_2', 'ddlink_2')">
+            <span class="icon">
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M11.5,1L2,6V8H21V6M16,10V17H19V10M2,22H21V19H2M10,10V17H13V10M4,10V17H7V10H4Z" />
+              </svg>
+            </span>
+            <span class="text">Financiero</span>
+          </a>
+          <ul id="ddmenu_2" class="dropdown-nav" style="{{ (request()->is('user/reports/*')) ? '' : 'display:none' }}">
+            <li >
+              <a href="/user/reports/customers" class="{{ (request()->is('user/reports/customers')) ? 'active' : '' }}">Entrada</a>
+            </li>
+            <li >
+              <a href="/user/reports/users" class="{{ (request()->is('user/reports/users')) ? 'active' : '' }}">Salida</a>
+            </li>
+          </ul>
         </li>
         @endcan
         <span class="divider">
@@ -34,7 +64,7 @@
         </span>
         @can('report-list')
         <li class="nav-item nav-item-has-children">
-          <a aria-expanded="false" class="collapsed" id="ddlink_1" href="#" onclick="toggle('ddmenu_1', 'ddlink_1')">
+          <a aria-expanded="false" class="collapsed" id="ddlink_3" href="#" onclick="toggle('ddmenu_3', 'ddlink_3')">
             <span class="icon">
               <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M19,3H14.82C14.25,1.44 12.53,0.64 11,1.2C10.14,1.5 9.5,2.16 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3M7,7H17V5H19V19H5V5H7V7M17,11H7V9H17V11M15,15H7V13H15V15Z" />
@@ -42,7 +72,7 @@
             </span>
             <span class="text">Relatorios</span>
           </a>
-          <ul id="ddmenu_1" class="dropdown-nav" style="{{ (request()->is('user/reports/*')) ? '' : 'display:none' }}">
+          <ul id="ddmenu_3" class="dropdown-nav" style="{{ (request()->is('user/reports/*')) ? '' : 'display:none' }}">
             <li >
               <a href="/user/reports/customers" class="{{ (request()->is('user/reports/customers')) ? 'active' : '' }}">Clientes</a>
             </li>
@@ -58,7 +88,7 @@
         </span>
 
         <li class="nav-item nav-item-has-children">
-          <a aria-expanded="false" class="collapsed" id="ddlink_2" href="#" onclick="toggle('ddmenu_2', 'ddlink_2')">
+          <a aria-expanded="false" class="collapsed" id="ddlink_4" href="#" onclick="toggle('ddmenu_4', 'ddlink_4')">
             <span class="icon">
               <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
@@ -66,7 +96,7 @@
             </span>
             <span class="text">Ajustes</span>
           </a>
-          <ul id="ddmenu_2" class="dropdown-nav" style="{{ (request()->is('user/users')) || (request()->is('user/ACL/*')) ? '' : 'display:none'}}">
+          <ul id="ddmenu_4" class="dropdown-nav" style="{{ (request()->is('user/users')) || (request()->is('user/ACL/*')) ? '' : 'display:none'}}">
             @can('user-list')
             <li>
               <a href="/user/users" class="{{ (request()->is('user/users')) ? 'active' : '' }}">
@@ -76,10 +106,10 @@
             @endcan
             @can('role-list','permission-list')
             <li>
-              <a aria-expanded="false" class="collapsed" id="ddlink_3" href="#" onclick="toggle('ddmenu_3', 'ddlink_3')">
+              <a aria-expanded="false" class="collapsed" id="ddlink_5" href="#" onclick="toggle('ddmenu_5', 'ddlink_5')">
                 <span class="text">ACL</span>
               </a>
-              <ul id="ddmenu_3" class="dropdown-nav" style="{{ (request()->is('user/ACL/*')) ? '' : 'display:none' }}">
+              <ul id="ddmenu_5" class="dropdown-nav" style="{{ (request()->is('user/ACL/*')) ? '' : 'display:none' }}">
                 @can('role-list')
                 <li>
                   <a href="/user/ACL/roles" class="{{ (request()->is('user/ACL/roles')) ? 'active' : '' }}"><span class="text">Roles</span></a>
@@ -110,6 +140,46 @@
       }else{
         n.style.display = '';
         document.getElementById(ddlink_1).setAttribute('aria-expanded', 'true');
+      }
+    }
+    function toggle(ddmenu_2, ddlink_2) {
+      var n = document.getElementById(ddmenu_2);
+      if (n.style.display != 'none'){
+        n.style.display = 'none';
+        document.getElementById(ddlink_2).setAttribute('aria-expanded', 'false');
+      }else{
+        n.style.display = '';
+        document.getElementById(ddlink_2).setAttribute('aria-expanded', 'true');
+      }
+    }
+    function toggle(ddmenu_3, ddlink_3) {
+      var n = document.getElementById(ddmenu_3);
+      if (n.style.display != 'none'){
+        n.style.display = 'none';
+        document.getElementById(ddlink_3).setAttribute('aria-expanded', 'false');
+      }else{
+        n.style.display = '';
+        document.getElementById(ddlink_3).setAttribute('aria-expanded', 'true');
+      }
+    }
+    function toggle(ddmenu_4, ddlink_4) {
+      var n = document.getElementById(ddmenu_4);
+      if (n.style.display != 'none'){
+        n.style.display = 'none';
+        document.getElementById(ddlink_4).setAttribute('aria-expanded', 'false');
+      }else{
+        n.style.display = '';
+        document.getElementById(ddlink_4).setAttribute('aria-expanded', 'true');
+      }
+    }
+    function toggle(ddmenu_5, ddlink_5) {
+      var n = document.getElementById(ddmenu_5);
+      if (n.style.display != 'none'){
+        n.style.display = 'none';
+        document.getElementById(ddlink_5).setAttribute('aria-expanded', 'false');
+      }else{
+        n.style.display = '';
+        document.getElementById(ddlink_5).setAttribute('aria-expanded', 'true');
       }
     }
   </script>
